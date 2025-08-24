653d5fee5a3affff6371702939820e7dac51e676
Igor de Freitas Cruz <igor.freitas.cruz@icloud.com>
2025-08-24 20:27:31 -0300

    config: updated sentry-sdk for FastAPI

--- src/main/server/app.py
+++ src/main/server/app.py
@@ -15,13 +15,9 @@ def init_sentry():
     sentry_dsn = os.environ['SENTRY_DSN']
     sentry_sdk.init(
         dsn=sentry_dsn,
-        # Set traces_sample_rate to 1.0 to capture 100%
-        # of transactions for performance monitoring.
-        traces_sample_rate=1.0,
-        # Set profiles_sample_rate to 1.0 to profile 100%
-        # of sampled transactions.
-        # We recommend adjusting this value in production.
-        profiles_sample_rate=1.0,
+        # Add data like request headers and IP for users,
+        # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+        send_default_pii=True,
     )
 
 
